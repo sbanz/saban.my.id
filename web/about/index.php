b1736efd25b796265a5ba9bec7087d4ab386d9a9
MirrorBottle <setiawanbayu66152@gmail.com>
2022-10-09 20:04:42 +0800

    fix: about

--- web/about/index.php
+++ web/about/index.php
@@ -5,8 +5,8 @@
         <div class="row align-items-center">
             <div class="col-lg-5 mx-auto text-center pt-4">
                 <img src="../../assets/img/WhatsApp Image 2022-10-09 at 12.03.40.jpeg" alt="" class="img-fluid img-thumbnail mb-3" style="height: 300px;">
-                <h1 class="text-white">Reihan Al'Syaban</h1>
-                <p><span>2109106002</span> <span class="mx-3">&bullet;</span> <span>Informatika A 21</span></p>
+                <h1 class="text-white">Reihan Al Sya'ban</h1>
+                <p><span>2109106051</span> <span class="mx-3">&bullet;</span> <span>Informatika A 21</span></p>
             </div>
         </div>
     </div>
